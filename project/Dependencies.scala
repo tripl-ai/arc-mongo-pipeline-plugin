d09006454361bee9a7c90b352eeaff09ae62ed84
Mike Seddon <seddonm1@gmail.com>
2020-03-20 22:16:24 +1100

    bump versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "2.4.4"
+  lazy val sparkVersion = "2.4.5"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "2.0.1" % "provided"
+  val arc = "ai.tripl" %% "arc" % "2.8.0" % "provided"
   val typesafeConfig = "com.typesafe" % "config" % "1.3.1" intransitive()  
 
   // spark
