49a2d5082e28a266e01ba0efdaec97f9a5bdffb4
Mike Seddon <seddonm1@gmail.com>
2020-09-04 13:07:27 +1000

    1.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,14 +8,14 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "3.1.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.2.0" % "provided"
   val typesafeConfig = "com.typesafe" % "config" % "1.3.1" intransitive()
 
   // spark
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
   // mongo
-  val mongo = "org.mongodb.spark" %% "mongo-spark-connector" % "2.4.2"
+  val mongo = "org.mongodb.spark" %% "mongo-spark-connector" % "3.0.0"
 
   // Project
   val etlDeps = Seq(
