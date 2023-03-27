006ea0c84221833c3f136b6920f8ee0c15ed9955
John Bruce <john.a.m.bruce@gmail.com>
2023-03-26 20:47:40 -0400

    Update to Arc 3.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "3.0.1"
+  lazy val sparkVersion = "3.3.2"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "3.7.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.13.2" % "provided"
 
   // spark
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
   // mongo
-  val mongo = "org.mongodb.spark" %% "mongo-spark-connector" % "3.0.0"
+  val mongo = "org.mongodb.spark" %% "mongo-spark-connector" % "3.0.2"
 
   // Project
   val etlDeps = Seq(
